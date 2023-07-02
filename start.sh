06e29e678749548ab2aac83525a28e434b2b30fe
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2023-07-02 19:25:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /file-filter-botv5
 fi
-cd /file-filter-bot
+cd /file-filter-botv5
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
